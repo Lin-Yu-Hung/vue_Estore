7f64d0265e00522b90f14ab1d5ff59e8ec9313e9
林昱鋐 <yuhunglin@lafresh.com.tw>
2023-02-20 11:17:41 +0800

    新增dashboard

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
 import * as bootstrap from 'bootstrap'
 
 import './style/all.scss'
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "dashboard",
+      component: () => import('@/views/dashboard/Index.vue'),
     },
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
   ],
