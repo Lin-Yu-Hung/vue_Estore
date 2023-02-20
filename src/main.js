5f2b057b7105d571af3afa1d3e446944dbca0868
林昱鋐 <yuhunglin@lafresh.com.tw>
2023-02-20 11:07:44 +0800

    bootstrap建置

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import * as bootstrap from 'bootstrap'
+
+import './style/all.scss'
 
 const app = createApp(App);
 
