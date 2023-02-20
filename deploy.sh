f5f8aea504f2b2615c936a86e4e0753dacadbde7
林昱鋐 <yuhunglin@lafresh.com.tw>
2023-02-20 10:32:58 +0800

    環境初步建置

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m '環境初步建置'
+
+# if you are deploying to https://4080e068.github.io
+# git push -f git@github.com:4080e068/4080e068.github.io.git main
+
+# if you are deploying to https://4080e068.github.io/vite
+git push -f https://github.com/4080E68/vue_Estore.git
+
+cd -
