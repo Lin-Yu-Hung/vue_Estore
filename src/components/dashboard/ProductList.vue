0dfee233852773d700bcc7f5c4d022f5e86648dc
林昱鋐 <yuhunglin@lafresh.com.tw>
2023-06-09 13:40:58 +0800

    新增頁面切換功能

--- src/components/dashboard/ProductList.vue
+++ src/components/dashboard/ProductList.vue
@@ -9,76 +9,93 @@
     新增商品
   </button>
   <div class="productList">
-    <table class="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">商品分類</th>
-          <th scope="col">商品圖片</th>
-          <th scope="col">商品名稱</th>
-          <th scope="col">商品原價</th>
-          <th scope="col">商品價格</th>
-          <th scope="col">是否啟用</th>
-          <th scope="col">編輯商品</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="product in productList" :key="product.id">
-          <td>{{ product.category }}</td>
-          <td class="imageItem">
-            <img :src="product.imageUrl" :alt="product.title" />
-          </td>
-          <td>{{ product.title }}</td>
-          <td>{{ product.origin_price.toLocaleString() }}</td>
-          <td>{{ product.price.toLocaleString() }}</td>
-          <td>
-            <div class="form-check form-switch switch-btn">
-              <input
-                class="form-check-input"
-                type="checkbox"
-                role="switch"
-                id="flexSwitchCheckDefault"
-                :checked="product.is_enabled == 1"
-              />
-            </div>
-          </td>
-          <td>
-            <div class="d-flex justify-content-center">
-              <button
-                type="button"
-                class="btn btn-outline-secondary py-2 px-3 mx-1"
-                data-bs-toggle="modal"
-                data-bs-target="#setProductModal"
-                @click="setStatus = 'edit'"
-              >
-                <font-awesome-icon icon="fa-pen-to-square" />編輯
-              </button>
-              <button
-                type="button"
-                class="btn btn-outline-danger py-2 px-3 mx-1"
-                @click="deleteProduct(product)"
-              >
-                <font-awesome-icon icon="fa-trash-can" />刪除
-              </button>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="table-responsive">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">商品分類</th>
+            <th scope="col">商品圖片</th>
+            <th scope="col">商品名稱</th>
+            <th scope="col">商品原價</th>
+            <th scope="col">商品價格</th>
+            <th scope="col">是否啟用</th>
+            <th scope="col">編輯商品</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in showData" :key="product.id">
+            <td>{{ product.category }}</td>
+            <td class="imageItem">
+              <img :src="product.imageUrl1" :alt="product.title" />
+            </td>
+            <td>{{ product.title }}</td>
+            <td>{{ product.origin_price.toLocaleString() }}</td>
+            <td>{{ product.price.toLocaleString() }}</td>
+            <td>
+              <div class="form-check form-switch switch-btn">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  role="switch"
+                  id="flexSwitchCheckDefault"
+                  :checked="product.is_enabled == 1"
+                />
+              </div>
+            </td>
+            <td>
+              <div class="d-flex justify-content-center">
+                <button
+                  type="button"
+                  class="btn btn-outline-secondary py-2 px-3 mx-1"
+                  data-bs-toggle="modal"
+                  data-bs-target="#setProductModal"
+                  @click="setStatus = 'edit'"
+                >
+                  <font-awesome-icon icon="fa-pen-to-square" />編輯
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-outline-danger py-2 px-3 mx-1"
+                  @click="deleteProduct(product)"
+                >
+                  <font-awesome-icon icon="fa-trash-can" />刪除
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 
   <div class="d-end">
     <nav aria-label="Page navigation example ">
       <ul class="pagination">
         <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
+          <a
+            class="page-link"
+            aria-label="Previous"
+            @click="nowPage--"
+            :class="{ disabled: nowPage == 1 }"
+          >
             <span aria-hidden="true">&laquo;</span>
           </a>
         </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item" v-for="(item, index) in pageCount" :key="index">
+          <a
+            class="page-link"
+            @click="switchpage(item)"
+            :class="{ active: nowPage == item }"
+            >{{ item }}</a
+          >
+        </li>
         <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
+          <a
+            class="page-link"
+            aria-label="Next"
+            @click="nowPage++"
+            :class="{ disabled: nowPage == pageCount }"
+          >
             <span aria-hidden="true">&raquo;</span>
           </a>
         </li>
@@ -89,26 +106,49 @@
 </template>
 <script>
 import { apiGetProductAll, apiDeleteProduct } from "@/api/api";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import loadingStore from "@/stores/loading";
 import SetProductModal from "../modal/SetProductModal.vue";
 import { deleteProductAlert } from "@/methods/sweetAlert.js";
+
 export default {
   components: { SetProductModal },
   setup() {
+    const maxDataLen = ref(2);
+    const nowPage = ref(1);
     const setStatus = ref("add");
     const loading = loadingStore();
     const productList = ref();
+
+    const pageCount = computed(() => {
+      if (productList.value) {
+        return Math.ceil(
+          Object.keys(productList.value).length / maxDataLen.value
+        );
+      } else {
+        return 0;
+      }
+    });
+    const showData = computed(() => {
+      if (productList.value) {
+        const startData =
+          nowPage.value * maxDataLen.value - maxDataLen.value + 1;
+        const endData = nowPage.value * maxDataLen.value;
+        return productList.value.slice(startData - 1, endData);
+      } else {
+        return [];
+      }
+    });
+
     const getProductAll = async () => {
       loading.showLoading();
       try {
         const res = await apiGetProductAll();
-        productList.value = res.data.products;
+        productList.value = Object.values(res.data.products);
       } catch (err) {
         console.log(err);
-      } finally {
-        loading.hideLoading();
       }
+      loading.hideLoading();
     };
     const deleteProduct = async (product) => {
       const result = await deleteProductAlert(product.title);
@@ -125,13 +165,20 @@ export default {
         loading.hideLoading();
       }
     };
-
+    // 切換頁數
+    const switchpage = (page) => {
+      nowPage.value = page;
+    };
     getProductAll();
+
     return {
-      productList,
+      showData,
       setStatus,
       getProductAll,
       deleteProduct,
+      pageCount,
+      nowPage,
+      switchpage,
     };
   },
 };
@@ -158,6 +205,7 @@ table {
   th {
     vertical-align: middle;
     text-align: center;
+    white-space: nowrap;
   }
   thead {
     th {
