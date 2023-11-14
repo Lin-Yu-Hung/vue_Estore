9cfe4dbba36e549f10ccf2921484f57675a7489d
林昱鋐 <yuhunglin@lafresh.com.tw>
2023-11-14 11:54:34 +0800

    [修改] 優化左側選單collapse狀態動畫

--- src/components/dashboard/Menu.vue
+++ src/components/dashboard/Menu.vue
@@ -17,9 +17,9 @@
         class="dropdown-title"
         @click="toggleStatus"
         data-bs-toggle="collapse"
-        data-bs-target="#test"
-        aria-expanded="true"
-        aria-controls="test"
+        data-bs-target="#productSetting"
+        aria-expanded="false"
+        aria-controls="productSetting"
       >
         <span class="pe-none">
           <font-awesome-icon
@@ -33,14 +33,14 @@
           icon="fa-chevron-right"
         />
       </div>
-      <div class="collapse multi-collapse" id="test">
+      <div class="collapse multi-collapse" id="productSetting">
         <ul>
-          <li class="pt-3 pb-0">
-            <router-link class="menu-link lg-down-hide" to="home"
+          <!-- <li class="collapse-item" parentId="productSetting">
+            <router-link class="menu-link lg-down-hide" to="createProduct"
               >建立商品</router-link
             >
-          </li>
-          <li class="pt-3 pb-0">
+          </li> -->
+          <li class="collapse-item" parentId="productSetting">
             <router-link class="menu-link lg-down-hide" to="productList"
               >商品列表</router-link
             >
@@ -48,6 +48,37 @@
         </ul>
       </div>
     </li>
+    <!-- <li class="dropdown-list lg-down-hide">
+      <div
+        class="dropdown-title"
+        @click="toggleStatus"
+        data-bs-toggle="collapse"
+        data-bs-target="#test1"
+        aria-expanded="false"
+        aria-controls="test1"
+      >
+        <span class="pe-none">
+          <font-awesome-icon
+            class="lg-down-hide"
+            icon="fa-list"
+            role="button"
+          />產品設定
+        </span>
+        <font-awesome-icon
+          class="lg-down-hide dropdown-arrow pe-none"
+          icon="fa-chevron-right"
+        />
+      </div>
+      <div class="collapse multi-collapse" id="test1">
+        <ul>
+          <li class="collapse-item" parentId="test1">
+            <router-link class="menu-link lg-down-hide" to="orderManage"
+              >購物車</router-link
+            >
+          </li>
+        </ul>
+      </div>
+    </li> -->
 
     <li>
       <router-link class="menu-link lg-down-hide" to="orderManage"
@@ -61,20 +92,96 @@
   </ul>
 </template>
 <script>
-import { ref } from "vue";
+import { ref, onMounted, watch, nextTick } from "vue";
+import { useRoute } from "vue-router";
+import { bsCollapse } from "@/methods/bootstrap.js";
 
 export default {
   setup() {
-    const active = ref("");
+    const route = useRoute();
+    const collapseMap = {};
+    const isClickableMap = {};
+    watch(
+      () => route.path,
+      (value) => {
+        setActicePage();
+      }
+    );
     const toggleStatus = (event) => {
-      event.target.classList.toggle("collapsed");
+      const controls = event.target.getAttribute("aria-controls");
+      if (isClickableMap[controls] === undefined) {
+        isClickableMap[controls] = true;
+      }
+      if (isClickableMap[controls]) {
+        event.target.classList.toggle("show");
+      }
+      isClickableMap[controls] = false;
+      setTimeout(() => {
+        isClickableMap[controls] = true;
+      }, 350); // bootstrap 預設動畫時間為0.35秒
     };
+    const setOtherCollapse = (status, targetId = null) => {
+      if (targetId) {
+        for (const key in collapseMap) {
+          if (key !== targetId) {
+            // 操作除了自身外的collapse
+            collapseMap[key][status]();
+          }
+        }
+      } else {
+        for (const key in collapseMap) {
+          // 操作所有collapse
+          collapseMap[key][status]();
+        }
+      }
+    };
+    const setActicePage = async () => {
+      // 尋找active頁面
+      await nextTick();
+      const activeElement = document.querySelector(".router-link-active");
+      const liElement = activeElement.parentElement; // li元素
+      const dropdownTitle = document.querySelectorAll(".dropdown-title");
+      dropdownTitle.forEach((el) => {
+        // 先將全部dropdownTitle移除顯示狀態，再由下面程式碼判斷當前頁面為何，並加上顯示狀態
+        el.classList.remove("show");
+        el.classList.remove("active");
+      });
+      if (liElement.classList.contains("collapse-item")) {
+        const liParentId = liElement.getAttribute("parentId");
+        const parentCollapse = document.querySelector(
+          // 透過parentId取得最上層的collapse元素
+          `[aria-controls=${liParentId}]`
+        );
+        setOtherCollapse("hide", liParentId);
+        // 添加顯示樣式
+        parentCollapse.classList.add("active");
+        parentCollapse.classList.add("show");
+        collapseMap[liParentId].show();
+      } else {
+        setOtherCollapse("hide");
+      }
+    };
+
+    onMounted(() => {
+      const allCollapsed = document.querySelectorAll(".collapse");
+      allCollapsed.forEach((collapse) => {
+        collapseMap[collapse.id] = bsCollapse(collapse.id);
+      });
+      setActicePage();
+    });
     return {
-      active,
       toggleStatus,
     };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.collapse-item {
+  padding-top: 16px;
+  padding-bottom: 0;
+  &::before {
+    content: "－";
+  }
+}
+</style>
--- src/methods/bootstrap.js
+++ src/methods/bootstrap.js
@@ -1,3 +1,4 @@
-import { Modal } from "bootstrap";
+import { Modal, Collapse } from "bootstrap";
 
 export const bsModal = (id) => new Modal(document.getElementById(id));
+export const bsCollapse = (id) => new Collapse(document.getElementById(id), { toggle: false });
