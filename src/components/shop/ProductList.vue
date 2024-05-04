5abbcd4023b799f3e2b195f9b7d6c569e7409de4
林昱鋐 <yuhunglin@lafresh.com.tw>
2024-05-04 10:31:30 +0800

    [新增] 新增商品列表讀取畫面

--- src/components/shop/ProductList.vue
+++ src/components/shop/ProductList.vue
@@ -8,61 +8,93 @@
   </nav>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-lg-3 pt-3">
+      <div class="col-lg-3 pt-3 ps-lg-0">
         <div class="card shadow p-3">
           <h2 class="fs-5 border-bottom pb-2">分類</h2>
-          <div
-            class="form-check mb-2"
-            v-for="category in productCategoryList"
-            :key="category"
-          >
-            <input
-              class="form-check-input"
-              type="checkbox"
-              :value="category"
-              :id="category"
-              name="category"
-              role="button"
-            />
-            <label class="form-check-label" role="button" :for="category">
-              {{ category }}
-            </label>
-          </div>
-
-          <label
-            for="customRange1"
-            class="form-label fs-5 border-bottom pb-2 mt-3"
-            >價格</label
-          >
-
-          <div class="mb-3 d-center flex-column">
-            <div class="d-flex">
-              <label
-                for="minPrice"
-                class="form-label mb-1 fs-small flex-grow-1 me-4"
-                >最低價格</label
-              >
-              <label for="maxPrice" class="form-label mb-1 fs-small flex-grow-1"
-                >最高價格</label
-              >
-            </div>
-            <div class="d-flex">
+          <template v-if="productCategoryList.length > 0">
+            <div
+              class="form-check mb-2"
+              v-for="category in productCategoryList"
+              :key="category"
+            >
               <input
-                type="number"
-                class="form-control form-control-sm"
-                id="minPrice"
-                placeholder="請輸入最低價格"
+                class="form-check-input"
+                type="checkbox"
+                :value="category"
+                :id="category"
+                name="category"
+                role="button"
               />
-              <span class="mx-1">~</span>
-              <input
-                type="number"
-                class="form-control form-control-sm"
-                id="maxPrice"
-                placeholder="請輸入最高價格"
-              />
+              <label class="form-check-label" role="button" :for="category">
+                {{ category }}
+              </label>
             </div>
-            <button class="btn btn-primary-median mt-3">篩選</button>
-          </div>
+
+            <label
+              for="customRange1"
+              class="form-label fs-5 border-bottom pb-2 mt-3"
+              >價格</label
+            >
+
+            <div class="mb-3 d-center flex-column">
+              <div class="d-flex">
+                <label
+                  for="minPrice"
+                  class="form-label mb-1 fs-small flex-grow-1 me-4"
+                  >最低價格</label
+                >
+                <label
+                  for="maxPrice"
+                  class="form-label mb-1 fs-small flex-grow-1"
+                  >最高價格</label
+                >
+              </div>
+              <div class="d-flex">
+                <input
+                  type="number"
+                  class="form-control form-control-sm"
+                  id="minPrice"
+                  placeholder="請輸入最低價格"
+                />
+                <span class="mx-1">~</span>
+                <input
+                  type="number"
+                  class="form-control form-control-sm"
+                  id="maxPrice"
+                  placeholder="請輸入最高價格"
+                />
+              </div>
+              <button class="btn btn-primary-median mt-3">篩選</button>
+            </div>
+          </template>
+          <template v-else>
+            <!-- 取得資料狀態 -->
+            <div class="placeholder-glow">
+              <span class="placeholder mb-2 col-8"></span>
+              <span class="placeholder mb-2 col-6"></span>
+            </div>
+            <div class="placeholder-glow">
+              <span class="placeholder mb-2 col-5"></span>
+            </div>
+            <div class="placeholder-glow">
+              <span class="placeholder mb-2 col-7"></span>
+              <span class="placeholder mb-2 col-7"></span>
+              <span class="placeholder mb-2 col-6"></span>
+              <span class="placeholder mb-2 col-8"></span>
+              <span class="placeholder mb-2 col-8"></span>
+              <span class="placeholder mb-2 col-10"></span>
+            </div>
+            <h2 class="fs-5 border-bottom pb-2 mt-3">價格</h2>
+            <div class="placeholder-glow">
+              <div class="d-between align-items-center">
+                <span class="placeholder col p-3 rounded"></span>
+                <span class="col-1 d-center">~</span>
+                <span class="placeholder col p-3 rounded"></span>
+              </div>
+
+              <span class="placeholder col-12 p-2 rounded mt-3">篩選</span>
+            </div>
+          </template>
         </div>
       </div>
       <div class="col-lg pt-3 mobile-px-0">
@@ -123,42 +155,72 @@
             </div>
           </div>
 
-          <TransitionGroup
-            name="list"
-            tag="div"
-            class="row row-cols-2 row-cols-md-3 row-cols-xxxl-4 g-3"
-          >
-            <div class="col" v-for="product in showData" :key="product.id">
-              <div class="card">
-                <div class="d-center">
-                  <img
-                    :src="product.imageUrl"
-                    class="card-img-top product-img"
-                    :alt="product.title"
-                    :title="product.title"
-                  />
-                </div>
+          <template v-if="productList.length > 0">
+            <TransitionGroup
+              name="list"
+              tag="div"
+              class="row row-cols-2 row-cols-md-3 row-cols-xxxl-4 g-3"
+            >
+              <div class="col" v-for="product in showData" :key="product.id">
+                <div class="card">
+                  <div class="d-center">
+                    <img
+                      :src="product.imageUrl"
+                      class="card-img-top product-img"
+                      :alt="product.title"
+                      :title="product.title"
+                    />
+                  </div>
 
-                <div class="card-body px-2 px-sm-3">
-                  <h5 class="card-title fs-5 line-clamp-2">
-                    {{ product.title }}
-                  </h5>
-                  <p
-                    class="card-text fs-small text-secondary line-clamp-2 mb-1"
-                  >
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <p class="text-end mb-2 fs-5" v-if="product.price">
-                    $ {{ product.price.toLocaleString() }}
-                  </p>
-                  <button class="btn btn-primary-median w-100">
-                    加入購物車
-                  </button>
+                  <div class="card-body px-2 px-sm-3">
+                    <h5 class="card-title fs-5 line-clamp-2">
+                      {{ product.title }}
+                    </h5>
+                    <p
+                      class="card-text fs-small text-secondary line-clamp-2 mb-1"
+                    >
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <p class="text-end mb-2 fs-5" v-if="product.price">
+                      $ {{ product.price.toLocaleString() }}
+                    </p>
+                    <button class="btn btn-primary-median w-100">
+                      加入購物車
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </TransitionGroup>
+          </template>
+          <template v-else>
+            <div class="row row-cols-2 row-cols-md-3 row-cols-xxxl-4 g-3">
+              <div class="col" v-for="item in 8" :key="item">
+                <div class="card" aria-hidden="true">
+                  <div
+                    class="card-img-top bg-secondary"
+                    style="height: 180px"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title placeholder-glow">
+                      <span class="placeholder col-6"></span>
+                    </h5>
+                    <p class="card-text placeholder-glow">
+                      <span class="placeholder col-7"></span>
+                      <span class="placeholder col-4"></span>
+                      <span class="placeholder col-4"></span>
+                      <span class="placeholder col-6"></span>
+                      <span class="placeholder col-8"></span>
+                    </p>
+                    <a
+                      class="btn btn-primary disabled placeholder col-6"
+                      aria-disabled="true"
+                    ></a>
+                  </div>
                 </div>
               </div>
             </div>
-          </TransitionGroup>
+          </template>
         </div>
       </div>
     </div>
@@ -197,6 +259,7 @@ export default {
       return sortProductList(filterResult, sortType.value);
     });
     return {
+      productList,
       productCategoryList,
       sortType,
       showType,
